e30dc1b392e7b260922302fb8641a0a1eef400fc
Vladislav <gto1103@gmail.com>
2023-02-07 23:51:02 +0300

    OK

---
+++ README.md
@@ -0,0 +1,9 @@
+# Webpack5
+
+[Информация о релизе Webpack 5](https://webpack.js.org/blog/2020-10-10-webpack-5-release/)
+
+[Migration Guide](https://webpack.js.org/migrate/5/)
+
+Status CI: [![Build status](https://ci.appveyor.com/api/projects/status/wjqoy8a68sa2fhus?svg=true)](https://ci.appveyor.com/project/Gto1103/ahj-dom)
+
+[Git Pages](https://gto1103.github.io/AHJ-DOM/)
---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-console */
+import validateInput from './validateInput';
+import addMessage from './addMessage';
+
+const textarea = document.querySelector('.textarea');
+const messages = document.querySelector('.messages');
+const modal = document.querySelector('.modal');
+const ok = document.querySelector('.ok');
+const cancel = document.querySelector('.cancel');
+const input = document.querySelector('.popup-text');
+let coordinates;
+
+textarea.addEventListener('keydown', (event) => {
+  if (textarea.value === '') return;
+  if (event.code === 'Enter') {
+    coordinates = null;
+    textarea.setAttribute('readonly', 'true');
+    const successCallback = (position) => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      coordinates = document.createElement('p');
+      coordinates.innerText = `[${latitude}, ${longitude}]`;
+      addMessage(coordinates, textarea, messages);
+      textarea.value = '';
+      textarea.removeAttribute('readonly');
+    };
+    const errorCallback = (error) => {
+      console.error(error);
+      modal.classList.remove('hidden');
+    };
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }
+});
+
+cancel.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.add('hidden');
+  input.classList.toggle('not-valid');
+  textarea.removeAttribute('readonly');
+  input.value = '';
+});
+
+ok.addEventListener('click', (e) => {
+  e.preventDefault();
+  const coord = validateInput(input.value);
+  if (coord) {
+    coordinates = document.createElement('p');
+    coordinates.innerText = `[${coord.latitude}, ${coord.longitude}] 👀`;
+    addMessage(coordinates, textarea, messages);
+    textarea.value = '';
+    input.value = '';
+    input.classList.remove('not-valid');
+    modal.classList.add('hidden');
+    textarea.removeAttribute('readonly');
+  } else {
+    textarea.removeAttribute('readonly');
+    input.classList.add('not-valid');
+    input.value = '';
+  }
+});
